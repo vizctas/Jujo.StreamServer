842660cff982dfaf1c5e733283fa17d3bdf26509
Jozh <joshuagom@gmail.com>
2026-05-18 22:06:05 -0500

    fix(ci): remove build-ninja artifacts and update .gitignore
    - Untrack 161 accidentally committed build artifacts from build-ninja/
    - The submodule reference build-ninja/_deps/tomlplusplus-src was breaking
      actions/checkout submodule fetch in CI (no .gitmodules entry)
    - Add build-ninja/ to .gitignore to prevent recurrence
    - All legitimate source files (Playnite plugin, cmake configs) already
      exist at their proper locations in plugins/ and cmake/

--- build-ninja/cpack_artifacts/JujoStreamServerInstaller.AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-[assembly: AssemblyTitle("Jujo.Stream Server Installer")]
-[assembly: AssemblyDescription("Jujo.Stream Server Installer")]
-[assembly: AssemblyProduct("Jujo.Stream Server Installer")]
-[assembly: AssemblyCompany("Jujo.Stream")]
-[assembly: AssemblyVersion("1.0.28.0")]
-[assembly: AssemblyFileVersion("1.0.28.0")]
-[assembly: AssemblyInformationalVersion("v1.0.28-test.4-dirty")]
--- build-ninja/cpack_artifacts/_CPack_Packages/win64/WIX/Jujo.StreamServer/autostart/scripts/autostart-service.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-rem Set the service to auto-start
-sc config Jujo.Server start= auto
